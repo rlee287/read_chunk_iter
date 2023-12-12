7b30b801b12b7a7ca8e3279858687de43cd83100
rlee287 <rlee287@yahoo.com>
2023-12-11 17:43:03 -0800

    Benchmark reworking and addition of itertools wrapping as comparison

--- benches/chunked_file_read.rs
+++ benches/chunked_file_read.rs
@@ -1,8 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
 
+use itertools::Itertools;
+
 use read_chunk_iter::{ChunkedReaderIter, ThreadedChunkedReaderIter};
 
-use std::io::{Write, Seek};
+use std::io::{Read, BufReader, Write, Seek};
 use std::fs::File;
 
 use poly1305::Poly1305;
@@ -10,6 +12,12 @@ use poly1305::Poly1305;
 use tempfile::NamedTempFile;
 use poly1305::universal_hash::{KeyInit, UniversalHash};
 
+fn do_std_iter_bufread(file: File, chunk_size: usize) {
+    let bufread = BufReader::with_capacity(chunk_size, file);
+    for chunk in &bufread.bytes().chunks(chunk_size) {
+        assert_eq!(chunk.map(|x| x.unwrap()).collect::<Vec<_>>(), vec![0xf0; chunk_size]);
+    }
+}
 fn do_chunked_read(file: File, chunk_size: usize, multiplier: usize) {
     let read_iter = ChunkedReaderIter::new(file, chunk_size, chunk_size*multiplier);
     for chunk in read_iter {
@@ -47,19 +55,19 @@ fn criterion_benchmark_multiplier(c: &mut Criterion) {
     
     let temp_file_path = temp_file.path();
 
-    let mut group = c.benchmark_group("Verify_multiplier");
+    let mut group: criterion::BenchmarkGroup<'_, criterion::measurement::WallTime> = c.benchmark_group("(verify, filesize:2MiB, chunk:8192, multiplier:?)");
 
     for multiplier in [1,2,4,6,8] {
-        group.bench_with_input(BenchmarkId::new("simple_chunk_read", multiplier),
+        group.bench_with_input(BenchmarkId::new("simple", multiplier),
             &multiplier, |b, &mult| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
-                |file| do_chunked_read(file, black_box(16384), mult),
+                |file| do_chunked_read(file, black_box(8192), mult),
                 criterion::BatchSize::PerIteration);
         });
-        group.bench_with_input(BenchmarkId::new("threaded_chunk_read", multiplier),
+        group.bench_with_input(BenchmarkId::new("threaded", multiplier),
             &multiplier, |b, &mult| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
-                |file| do_threaded_chunked_read(file, black_box(16384), mult),
+                |file| do_threaded_chunked_read(file, black_box(8192), mult),
                 criterion::BatchSize::PerIteration);
         });
     }
@@ -74,19 +82,19 @@ fn criterion_benchmark_multiplier_hash(c: &mut Criterion) {
     
     let temp_file_path = temp_file.path();
 
-    let mut group = c.benchmark_group("hashing_multiplier");
+    let mut group = c.benchmark_group("(hash, filesize:2MiB, chunk:8192, multiplier:?)");
 
     for multiplier in [1,2,4,6,8] {
-        group.bench_with_input(BenchmarkId::new("simple_chunk_read", multiplier),
+        group.bench_with_input(BenchmarkId::new("simple", multiplier),
             &multiplier, |b, &mult| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
-                |file| do_chunked_read_hash(file, black_box(16384), mult),
+                |file| do_chunked_read_hash(file, black_box(8192), mult),
                 criterion::BatchSize::PerIteration);
         });
-        group.bench_with_input(BenchmarkId::new("threaded_chunk_read", multiplier),
+        group.bench_with_input(BenchmarkId::new("threaded", multiplier),
             &multiplier, |b, &mult| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
-                |file| do_threaded_chunked_read_hash(file, black_box(16384), mult),
+                |file| do_threaded_chunked_read_hash(file, black_box(8192), mult),
                 criterion::BatchSize::PerIteration);
         });
     }
@@ -94,7 +102,7 @@ fn criterion_benchmark_multiplier_hash(c: &mut Criterion) {
 }
 
 fn criterion_benchmark_filesize(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Verify_filesizes");
+    let mut group = c.benchmark_group("(verify, filesize:?, chunk:16384, multiplier:4)");
 
     for file_size in [128, 256, 512, 1024, 1024*2] {
         let mut temp_file = NamedTempFile::new().unwrap();
@@ -104,13 +112,13 @@ fn criterion_benchmark_filesize(c: &mut Criterion) {
         
         let temp_file_path = temp_file.path();
 
-        group.bench_with_input(BenchmarkId::new("simple_chunk_read", file_size),
+        group.bench_with_input(BenchmarkId::new("simple", file_size),
             &file_size, |b, &_size| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
                 |file| do_chunked_read(file, black_box(16384), black_box(4)),
                 criterion::BatchSize::PerIteration);
         });
-        group.bench_with_input(BenchmarkId::new("threaded_chunk_read", file_size),
+        group.bench_with_input(BenchmarkId::new("threaded", file_size),
             &file_size, |b, &_size| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
                 |file| do_threaded_chunked_read(file, black_box(16384), black_box(4)),
@@ -121,7 +129,7 @@ fn criterion_benchmark_filesize(c: &mut Criterion) {
 }
 
 fn criterion_benchmark_chunksize(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Verify_chunksizes");
+    let mut group = c.benchmark_group("(verify, filesize:2MiB, chunk:?, multiplier:4)");
 
     for chunk_size in [1024*2, 1024*4, 1024*8, 1024*16, 1024*32] {
         let mut temp_file = NamedTempFile::new().unwrap();
@@ -131,13 +139,19 @@ fn criterion_benchmark_chunksize(c: &mut Criterion) {
         
         let temp_file_path = temp_file.path();
 
-        group.bench_with_input(BenchmarkId::new("simple_chunk_read", chunk_size),
+        group.bench_with_input(BenchmarkId::new("iter_bufread", chunk_size),
+            &chunk_size, |b, &size| {
+            b.iter_batched(|| File::open(temp_file_path).unwrap(),
+                |file| do_std_iter_bufread(file, size*4),
+                criterion::BatchSize::PerIteration);
+        });
+        group.bench_with_input(BenchmarkId::new("simple", chunk_size),
             &chunk_size, |b, &size| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
                 |file| do_chunked_read(file, size, black_box(4)),
                 criterion::BatchSize::PerIteration);
         });
-        group.bench_with_input(BenchmarkId::new("threaded_chunk_read", chunk_size),
+        group.bench_with_input(BenchmarkId::new("threaded", chunk_size),
             &chunk_size, |b, &size| {
             b.iter_batched(|| File::open(temp_file_path).unwrap(),
                 |file| do_threaded_chunked_read(file, size, black_box(4)),
