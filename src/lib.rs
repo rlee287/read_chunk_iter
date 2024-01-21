70b8345d5d0a7034b4cf2bc4a2d7b1be8ff33adc
rlee287 <rlee287@yahoo.com>
2024-01-20 19:36:14 -0800

    Add some documentation to lib.rs from the README

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 //! Iterator adapters over a reader that yields fixed-size chunks at a time.
+//! These iterators are substantially more performant than using iterator 
+//! adapters over a `Bytes` iterator of a `Read` object.
 //!
 //! All the iterators in this crate will yield the exactly specified number of bytes, except under the following circumstances:
 //!  - EOF is reached, in which case a partial chunk is yielded. (This can occur multiple times if EOF is hit multiple times, e.g. with a network socket.)
 //!  - An IO error other than `ErrorKind::Interrupted` occurs, in which case a partial chunk is yielded before the error. This preserves the exact byte location at which an error occured.
+//!
+//! ## Features
+//! - autodetect_vectored: Enable automatic detection of whether vectored reads offer speedups, and take advantage of them when they offer speedups. This feature requires nightly, but manual selection of vectored reads is still possible without it.
 #![forbid(unsafe_code)]
 #![cfg_attr(feature = "autodetect_vectored", feature(can_vector))]
 
